2deec0e291c0abc751ccdc2142464365d4de2718
randeep <randeepsaggu@gmail.com>
2024-12-18 22:51:24 +0530

    resolved build issues

--- practice/Project1/Source.cpp
+++ practice/Project1/Source.cpp
@@ -1,5 +1,6 @@
 #include<vector>
 #include<iostream>
+#include<algorithm>
 using namespace std;
 
 void main() {
@@ -12,26 +13,26 @@ void main() {
 
 	//Time complexity: O(n)
 	//Space complexity : O(n)
-	std::vector<int> v(5);  // Vector of size 5 initialized with 0
+	std::vector<int> v1(5);  // Vector of size 5 initialized with 0
 
 	//Initialization with Initializer List
 	//Time complexity : O(n)
 	//Space complexity : O(n)
-	std::vector<int> v = { 1, 2, 3, 4, 5 };  // Initialize with values
+	std::vector<int> v2 = { 1, 2, 3, 4, 5 };  // Initialize with values
 
 
 	//copy constructor
 	//Time complexity: O(n)
 	//Space complexity : O(n)
-	std::vector<int> v1 = { 1, 2, 3 };
-	std::vector<int> v2(v1);  // Copy of v1
+	std::vector<int> v3 = { 1, 2, 3 };
+	std::vector<int> v4(v3);  // Copy of v1
 
 
 	//move constructor
 	//Time complexity : O(1)
 	//Space complexity : O(1)
-	std::vector<int> v1 = { 1, 2, 3 };
-	std::vector<int> v2(std::move(v1));  // v1's resources transferred to v2
+	std::vector<int> v5 = { 1, 2, 3 };
+	std::vector<int> v6(std::move(v5));  // v5's resources transferred to v6
 
 	//Size and capacity
 	//size
@@ -41,9 +42,9 @@ void main() {
 	//Time complexity: O(1)
 	//Space complexity: O(1)
 
-	std::vector<int> v = { 1, 2, 3 };
-	std::cout << "Size: " << v.size() << "\n";         // Outputs 3
-	std::cout << "Capacity: " << v.capacity() << "\n"; // Outputs >= 3
+	std::vector<int> v7 = { 1, 2, 3 };
+	std::cout << "Size: " << v7.size() << "\n";         // Outputs 3
+	std::cout << "Capacity: " << v7.capacity() << "\n"; // Outputs >= 3
 
 
 
@@ -51,47 +52,47 @@ void main() {
 	// reserve
 	//time O(n)
 	// space O(n)
-	std::vector<int> v;
-    v.reserve(10); // Allocate memory for 10 elements
-    std::cout << "Capacity: " << v.capacity() << std::endl;
+	std::vector<int> v8;
+    v8.reserve(10); // Allocate memory for 10 elements
+    std::cout << "Capacity: " << v8.capacity() << std::endl;
 
 	//push_back
 
 	//time O(1)
 	//space O(n)
-   std::vector<int> v;
-    v.push_back(10);
-    v.push_back(20);
-    for (int i : v) std::cout << i << " "; // Output: 10 20
+	std::vector<int> v9;
+    v9.push_back(10);
+    v9.push_back(20);
+    for (int i : v9) std::cout << i << " "; // Output: 10 20
 
 	//insert
 	//time : O(n)
 	//space: O(n)
-	std::vector<int> v = {1, 2, 3};
-    v.insert(v.begin() + 1, 99);
-    for (int i : v) std::cout << i << " "; // Output: 1 99 2 3
+	std::vector<int> v10 = {1, 2, 3};
+    v10.insert(v10.begin() + 1, 99);
+    for (int i : v10) std::cout << i << " "; // Output: 1 99 2 3
 
 
 	//erase
 	//time O(n)
 	//space O(1)
-	std::vector<int> v = {1, 2, 3};
-    v.erase(v.begin() + 1);
-    for (int i : v) std::cout << i << " "; // Output: 1 3
+	std::vector<int> v11 = {1, 2, 3};
+    v11.erase(v11.begin() + 1);
+    for (int i : v11) std::cout << i << " "; // Output: 1 3
 
 
 	//sort
 	//Time Complexity: O(nlogn), where n n is the number of elements.
 	//Space Complexity: O(logn) (for recursion stack space).
-    std::vector<int> v = {3, 1, 2};
-    std::sort(v.begin(), v.end());
-    for (int i : v) std::cout << i << " "; // Output: 1 2 3
+    std::vector<int> v12 = {3, 1, 2};
+    std::sort(v12.begin(), v.end());
+    for (int i : v12) std::cout << i << " "; // Output: 1 2 3
 
 	//swap
 	//time O(1)
 	//space O(1)
-	std::vector<int> v1 = {1, 2, 3};
-    std::vector<int> v2 = {4, 5, 6};
-    v1.swap(v2);
-    for (int i : v1) std::cout << i << " "; // Output: 4 5 6
+	std::vector<int> v13 = {1, 2, 3};
+    std::vector<int> v23 = {4, 5, 6};
+    v13.swap(v23);
+    for (int i : v13) std::cout << i << " "; // Output: 4 5 6
 }
